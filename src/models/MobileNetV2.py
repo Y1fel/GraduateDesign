c910c01a7f27453c8e46f7b104ec7ba159733df4
Y1fel <rechardifelly@gmail.com>
2026-03-09 18:38:52 +0800

    add MobileNetV2.py

---
+++ src/models/MobileNetV2.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torchvision.models.mobilenetv2 import InvertedResidual
+
+
+class MobileNetV2(nn.Module):
+    def __init__(self, num_classes=1000):
+        super().__init__()
+
+        cfg = [
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],
+        ]
+
+        layers = []
+
+        # first conv
+        input_channel = 32
+        layers.append(
+            nn.Sequential(
+                nn.Conv2d(3, input_channel, 3, 2, 1, bias=False),
+                nn.BatchNorm2d(input_channel),
+                nn.ReLU6(inplace=True)
+            )
+        )
+
+        # inverted residual blocks
+        for t, c, n, s in cfg:
+            for i in range(n):
+                stride = s if i == 0 else 1
+                layers.append(
+                    InvertedResidual(
+                        input_channel,
+                        c,
+                        stride,
+                        t
+                    )
+                )
+
+                input_channel = c
+        # last conv
+        layers.append(
+            nn.Sequential(
+                nn.Conv2d(input_channel, 1280, 1, bias=False),
+                nn.BatchNorm2d(1280),
+                nn.ReLU6(inplace=True)
+            )
+        )
+
+        self.features = nn.Sequential(*layers)
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.classifier = nn.Linear(1280, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.pool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
